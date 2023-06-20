b1809d9602a6ff56f3a282533ca40681180e2a4e
Lucas Santos <71449297+LucassSantoss@users.noreply.github.com>
2023-06-20 11:13:27 -0300

    main.py

--- main.py
+++ main.py
@@ -1,105 +1,94 @@
-# Feito por: Bruno Mascioli de Souza e Lucas Pereira dos Santos
 import math
 from random import choice
 
-def ler_exemplares():
-  matriz = []
-  arquivo = open('iris.data.csv', 'r')
-  for linha in arquivo:
-    dados = linha.strip().split(',')
-    matriz.append(dados)
-  arquivo.close()
-  return matriz[1:]
+def read_examples():
+    matrix = []
+    file = open('iris.data.csv', 'r')
+    for line in file:
+        data = line.strip().split(',')
+        matrix.append(data)
+    file.close()
+    return matrix[1:]
 
+def read_unclassified(file_name):
+    matrix = []
+    file = open(file_name, 'r')
+    for line in file:
+        data = line.strip().split(',')
+        matrix.append(data)
+    file.close()
+    return matrix
 
-def ler_sem_classificacao(nome_arquivo):
-  matriz = []
-  arquivo = open(nome_arquivo, 'r')
-  for linha in arquivo:
-    dados = linha.strip().split(',')
-    matriz.append(dados)
-  arquivo.close()
-  return matriz
+def euclidean_distance(known_example, unknown_example):
+    distance = math.sqrt(((float(known_example[0]) - float(unknown_example[0])) ** 2) +
+                         ((float(known_example[1]) - float(unknown_example[1])) ** 2) +
+                         ((float(known_example[2]) - float(unknown_example[2])) ** 2) +
+                         ((float(known_example[3]) - float(unknown_example[3])) ** 2))
+    return distance
 
-def distancia_euclidiana(exemplo, nao_classificados):
-  distancia = math.sqrt(((exemplo[0]) - (nao_classificados[0]))**2 +
-                            ((exemplo[1]) - (nao_classificados[1]))**2 +
-                            ((exemplo[2]) - (nao_classificados[2]))**2 +
-                            ((exemplo[3]) - (nao_classificados[3]))**2)
-  return distancia
+def calculate_distances(examples_matrix, unclassified_matrix, k):
+    matrix = []
+    for unknown_example in unclassified_matrix:
+        distances = []
+        unknown_example = list(map(float, unknown_example))
+        for known_example in examples_matrix:
+            species = known_example[4]
+            known_example = list(map(float, known_example[:-1]))
+            distance = euclidean_distance(known_example, unknown_example)
+            distances.append([distance, species, unknown_example])
+        sorted_distances = sorted(distances)
+        matrix.append(sorted_distances[:k])
+    return matrix
 
-def calculo_distancias(matriz_exemplares, matriz_sem_classificacao, k):
-  matriz = []
-  for nao_classificados in matriz_sem_classificacao:
-    distancias = []
-    nao_classificados = list(map(float, nao_classificados))
-    for exemplo in matriz_exemplares:
-      especie = exemplo[4]
-      exemplo = list(map(float, exemplo[:-1]))
-      distancia = distancia_euclidiana(exemplo, nao_classificados)
-      distancias.append([distancia, especie, nao_classificados])
-    distancias_ordenadas = sorted(distancias)
-    matriz.append(distancias_ordenadas[:k])
-  return matriz
-
-
-def determina_classe(matriz):
-  arquivo = open('iris.data.csv', 'a')
-  for especie_sem_classificacao in matriz:
-    setosa, versicolor, virginica = 0, 0, 0
-    for pontos in especie_sem_classificacao:
-      if pontos[1] == 'setosa':
-        setosa += 1
-      elif pontos[1] == 'versicolor':
-        versicolor += 1
-      else:
-        virginica += 1
-    arquivo.write('\n')
-    for elem in especie_sem_classificacao[0][2]:
-      elem = str(elem)
-      arquivo.write(elem)
-      arquivo.write(',')
-    #Casos em que existe um valor maior
-    if setosa > versicolor and setosa > virginica:
-      arquivo.write('setosa')
-      print(especie_sem_classificacao[0][2], 'setosa')
-    elif versicolor > setosa and versicolor > virginica:
-      arquivo.write('versicolor')
-      print(especie_sem_classificacao[0][2], 'versicolor')
-    elif virginica > setosa and virginica > versicolor:
-      arquivo.write('virginica')
-      print(especie_sem_classificacao[0][2], 'virginica')
-    #Casos de valores iguais
-    #todas iguais
-    elif virginica == versicolor == setosa:
-      sorteio = choice(['virginica','setosa', 'versicolor'])
-      arquivo.write(sorteio)
-      print(especie_sem_classificacao[0][2], sorteio)
-      #virginica = versicolor
-    elif virginica == versicolor > setosa:
-      sorteio = choice(['virginica','versicolor'])
-      arquivo.write(sorteio)
-      print(especie_sem_classificacao[0][2], sorteio)
-      #virginica = setosa
-    elif virginica == setosa > versicolor:
-      sorteio = choice(['virginica','setosa'])
-      arquivo.write(sorteio)
-      print(especie_sem_classificacao[0][2], sorteio)
-      #setosa = versicolor
-    else:
-      sorteio = choice(['setosa', 'versicolor'])
-      arquivo.write(sorteio)
-      print(especie_sem_classificacao[0][2], sorteio)
-  arquivo.close()
-
+def determine_class(matrix):
+    file = open('iris.data.csv', 'a')
+    for unclassified_species in matrix:
+        setosa, versicolor, virginica = 0, 0, 0
+        for points in unclassified_species:
+            if points[1] == 'setosa':
+                setosa += 1
+            elif points[1] == 'versicolor':
+                versicolor += 1
+            else:
+                virginica += 1
+        file.write('\n')
+        for elem in unclassified_species[0][2]:
+            elem = str(elem)
+            file.write(elem)
+            file.write(',')
+        if setosa > versicolor and setosa > virginica:
+            file.write('setosa')
+            print(unclassified_species[0][2], 'setosa')
+        elif versicolor > setosa and versicolor > virginica:
+            file.write('versicolor')
+            print(unclassified_species[0][2], 'versicolor')
+        elif virginica > setosa and virginica > versicolor:
+            file.write('virginica')
+            print(unclassified_species[0][2], 'virginica')
+        elif virginica == versicolor == setosa:
+            choice_value = choice(['virginica', 'setosa', 'versicolor'])
+            file.write(choice_value)
+            print(unclassified_species[0][2], choice_value)
+        elif virginica == versicolor > setosa:
+            choice_value = choice(['virginica', 'versicolor'])
+            file.write(choice_value)
+            print(unclassified_species[0][2], choice_value)
+        elif virginica == setosa > versicolor:
+            choice_value = choice(['virginica', 'setosa'])
+            file.write(choice_value)
+            print(unclassified_species[0][2], choice_value)
+        else:
+            choice_value = choice(['setosa', 'versicolor'])
+            file.write(choice_value)
+            print(unclassified_species[0][2], choice_value)
+    file.close()
 
 def main():
-  k = int(input('K: '))
-  nome_arquivo = input('Nome do arquivo de dados não classificados: ')
-  matriz_exemplares = ler_exemplares()
-  matriz_sem_classificacao = ler_sem_classificacao(nome_arquivo)
-  matriz = calculo_distancias(matriz_exemplares, matriz_sem_classificacao, k)
-  determina_classe(matriz)
-
+    k = int(input('K: '))
+    file_name = input('Name of the unclassified data file: ')
+    examples_matrix = read_examples()
+    unclassified_matrix = read_unclassified(file_name)
+    matrix = calculate_distances(examples_matrix, unclassified_matrix, k)
+    determine_class(matrix)
 
 main()
